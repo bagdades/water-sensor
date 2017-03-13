b64957ba1784d2e80ddd9bc8393d3fbc1158a6f3
bagdades <volodumurkoval0@gmail.com>
2017-03-13 22:19:05 +0200

    Add source

---
+++ src/inc.h
@@ -0,0 +1,19 @@
+/*
+ * inc.h
+ *
+ *  Created on: 11 лют. 2011
+ *      Author: vovan
+ */
+
+#ifndef INC_H_
+#define INC_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <avr/eeprom.h>
+#include <inttypes.h>
+#include <util/delay.h>
+
+
+#endif /* INC_H_ */
---
+++ src/main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  13.03.17 22:12:14
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  vovan (), volodumurkoval0@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+ #include "inc.h"
+
+int main(void)
+{
+	while(1);
+}
