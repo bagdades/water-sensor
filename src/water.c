becb5b09ce19348328bdd8bd1af3064ebf264e4b
bagdades <volodumurkoval0@gmail.com>
2017-03-18 22:41:14 +0200

    Work

--- src/water.c
+++ src/water.c
@@ -19,20 +19,34 @@
  #include "inc.h"
 
 
+void Init(void)
+{
+	asm("sei");
+}
+
+void TimerInit(void)
+{
+	TCCR0B |= (1 << CS02) | (1 << CS00); /* clkio/1024 */
+	TCNT0 = T0_INIT;
+	TIMSK0 |= (1 << TOIE0); /* overflow interrupt enable */
+}
+
 void ADCInit(void) 
 {
-#if(F_CPU != 8000000)
+#if(F_CPU != 9600000)
 #error ***You must set ADCSRA
 #endif
-	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADEN); //PRESC F_CPU/64
-	ADMUX &= ~ADC_VREF_TYPE; //vref
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1); //PRESC F_CPU/64
+	/* ADMUX &= (1 << REFS0); #<{(| set volage reference to Vcc |)}># */
 }
 
 int16_t ADCRead(uint8_t chanel) 
 {
-	ADMUX &= ~ADC_VREF_TYPE; //vref
-	ADMUX &= 0xF8;
+	ADMUX &= (1 << REFS0); /* set volage reference to Vcc */
+	ADMUX &= 0xFA;
+	chanel &= 0x03;
 	ADMUX |= chanel;
+	ADCSRA |= (1 << ADEN);
 	_delay_us(10); //delay needed for stabilization of the ACD input voltag
 	ADCSRA |= (1 << ADSC); //start convertion
 	while ((ADCSRA & (1 << ADIF)) == 0)
@@ -40,4 +54,9 @@ int16_t ADCRead(uint8_t chanel)
 	ADCSRA |= (1 << ADIF);
 	/* flag.adcRead = FALSE; */
 	return ADCW;
+}
+
+ISR(TIM0_OVF_vect)
+{
+	TCNT0 = T0_INIT;
 }
--- src/water.h
+++ src/water.h
@@ -4,10 +4,16 @@
 #include <inttypes.h>
 #include <avr/pgmspace.h>
 
-#define F_CPU			8000000UL
-#define ADC_VREF_TYPE ((1<<REFS1)|(1<<REFS0))
+#define F_CPU			9600000UL
+
+//Timer
+#define T0_PRESC	1024UL
+#define T0_FREQ		1000UL
+#define T0_INIT		255 - (F_CPU/(T0_FREQ * T0_PRESC))
 
 //Prototypes
+void Init(void);
+void TimerInit(void);
 void ADCInit(void);
 int16_t ADCRead(uint8_t chanel);
 
